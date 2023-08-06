1d77022cb97378a3e67c5f0c744ddb6e6cff4fca
noorduyn <njorthson@proton.me>
2023-08-06 14:03:03 +0200

    added nixmacVM

---
+++ hosts/nixmacVM/default.nix
@@ -0,0 +1,24 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  ...
+}:
+  {
+  imports = [
+    ../../modules/common
+    ./hardware-configuration.nix
+    ../../users/wash
+    ../../modules/desktop
+  ];
+
+  networking.hostName = "nixmacVM";
+  time.timeZone = "Europe/Amsterdam";
+
+  #For ZFS support
+#  networking.hostId = "fd91c922";
+
+  system.stateVersion = "23.05";
+}
