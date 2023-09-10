2107ad8a1c12594cb21474d335e6f838475fc2a4
Nordyun <njorthson@proton.me>
2023-09-10 20:21:17 +0200

    test

--- hosts/anubis/impermanence.nix
+++ hosts/anubis/impermanence.nix
@@ -8,25 +8,13 @@
     inputs.impermanence.nixosModules.impermanence
   ];
 
-#  filesystems
-#  fileSystems."/".options = ["compress=zstd" "noatime"];
-#  fileSystems."/home".options = ["compress=zstd" "noatime"];
-#  fileSystems."/nix".options = ["compress=zstd" "noatime"];
-#  fileSystems."/persist".options = ["compress=zstd" "noatime"];
-#  fileSystems."/persist".neededForBoot = true;
-#  fileSystems."/var/log".options = ["compress=zstd" "noatime"];
-#  fileSystems."/var/log".neededForBoot = true;
-
 ### TODO - break these security settings into seperate module
 
   fileSystems."/".options = [ "noexec" ];
   fileSystems."/etc/nixos".options = [ "noexec" ];
   fileSystems."/var/log".options = [ "noexec" ];
-  
+
 #  nix.settings.allowed-users = [ "@wheel" ];
-
-###
-
 
 # always persist these
   environment.persistence."/persist" = {
