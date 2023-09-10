abc2fd7674319d957c1f42243919880725164f21
Nordyun <njorthson@proton.me>
2023-09-10 22:05:36 +0200

    test10034

--- hm/desktop.nix
+++ hm/desktop.nix
@@ -13,11 +13,11 @@
     firefox
     _1password-gui
   ];
-  home.file."${config.xdg.configHome}" = {
+  home.file."${config.xdg.configHome}".ricerscripts = {
     source = ./ricerscripts;
     recursive = true;
   };
-  home.file."${config.xdg.configHome}" = {
+  home.file."${config.xdg.configHome}".wallpapers = {
     source = ./wallpapers;
     recursive = true;
   };
