2b40927120482b99b12b371e453dede0ea40b997
noorduyn <njorthson@proton.me>
2023-09-10 16:03:18 +0200

    new key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,7 +6,8 @@ let
   anubis = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFE4rG9LOLWZJq38YRZlK7lNK3lL8HYRn61sgp6CrDcJ";
   neptune = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeno7kGL0KKvPzaqLyP3TyXqUkM3M0Lphk9r94hut5t";
   zelda = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQ/40iaZdCUOK24lAyPmyt1SJVaLKGQK50FZCm5Mzbt";
-  systems = [ anubis neptune zelda ];
+  nixmacVM = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFdEzCPKX9ztDRMZuUAqnPuRB8ddc8Za61X3GKuIdauw";
+  systems = [ anubis neptune zelda nixmacVM ];
 in
 {
 #  "secret1.age".publicKeys = [ wash anubis ];
