3f703e8aa029db50a92b1959558187a9a3fde26c
noorduyn <njorthson@proton.me>
2023-11-02 17:08:36 +0100

    rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,7 +4,8 @@ let
   users = [ wash wyatt ];
 
   anubis = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFE4rG9LOLWZJq38YRZlK7lNK3lL8HYRn61sgp6CrDcJ";
-  neptune = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeno7kGL0KKvPzaqLyP3TyXqUkM3M0Lphk9r94hut5t";
+  neptuneOld = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeno7kGL0KKvPzaqLyP3TyXqUkM3M0Lphk9r94hut5t";
+  neptune = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANAv4dZ9dcuOm3mKPi+I/hdZeIE8TrovRxqV74yMKhS";
   zelda = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINQ/40iaZdCUOK24lAyPmyt1SJVaLKGQK50FZCm5Mzbt";
   nixmacVM = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL4dWVZcNnAXGKgF0ZlzGCIkD93pODqU05qH7RzhPIWv";
   systems = [ anubis neptune zelda nixmacVM ];
