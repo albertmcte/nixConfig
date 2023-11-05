fd5573a8a07e926a45f8d399bfde970dc25e9e1d
Nordyun <njorthson@proton.me>
2023-11-05 20:11:36 +0100

    zpool transfer

--- hosts/anubis/default.nix
+++ hosts/anubis/default.nix
@@ -5,6 +5,8 @@
     ./hardware-configuration.nix
     ./impermanence.nix  
     ./resilio.nix
+    ./sanoid.nix
+    ./syncoid.nix
     ../../users/wash
     ../../modules/desktop
     ../../modules/server
@@ -16,5 +18,11 @@
   #For ZFS support
   networking.hostId = "fd91c922";
   boot.zfs.extraPools = [ "mercury" ];
+
+  services.plex = {
+    enable = true;
+    openFirewall = true;
+  };
+
   system.stateVersion = "23.05";
 }
