a09fafbbf79efa9292f9e29e40893ac644a390ab
Nordyun <njorthson@proton.me>
2024-10-12 21:15:30 -0400

    fix firewall for nfs

--- hosts/anubis/default.nix
+++ hosts/anubis/default.nix
@@ -28,11 +28,19 @@
       enable = true;
       openFirewall = true;
     };
-    nfs.server.enable = true;
+    nfs.server = {
+      enable = true;
+      # fixed rpc.statd port; for firewall
+      lockdPort = 4001;
+      mountdPort = 4002;
+      statdPort = 4000;
+      extraNfsdConfig = '''';
+    };
   };
+
   networking.firewall.interfaces."enp5s0" = {
-    allowedTCPPorts = [ 2049 31225 ];
-    allowedUDPPorts = [ 31225 ];
+    allowedTCPPorts = [ 111 2049 4000 4001 4002 20048 31225 ];
+    allowedUDPPorts = [ 111 2049 4000 4001 4002 20048 31225 ];
   };
 
   system.stateVersion = "23.11";
