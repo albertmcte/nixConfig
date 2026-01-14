d6b11baadc5d3672088ce5316ab6383ad178e450
noorduyn <njorthson@proton.me>
2026-01-14 12:26:36 -0600

    nixmacvm test w hypr

--- hosts/nixmacVM/default.nix
+++ hosts/nixmacVM/default.nix
@@ -1,11 +1,38 @@
+{ pkgs, ... }:
 {
   imports = [
     ../../modules/common
     ./hardware-configuration.nix
     ../../users/wash
-    #    ../../modules/desktop
-    ../../modules/desktop/hyprwm.nix
   ];
+
+  # Hyprland from nixpkgs (not the flake version)
+  programs = {
+    dconf.enable = true;
+    hyprland = {
+      enable = true;
+      withUWSM = true;
+      # Uses nixpkgs hyprland by not specifying package
+    };
+  };
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Home-manager hyprland config with nixpkgs package
+  home-manager.users.wash = {
+    imports = [ ../../hm/hyprland.nix ];
+    wayland.windowManager.hyprland = {
+      package = pkgs.hyprland;
+      portalPackage = pkgs.xdg-desktop-portal-hyprland;
+      plugins = []; # Flake plugins incompatible with nixpkgs hyprland
+    };
+  };
 
   networking = {
     hostName = "nixmacVM";
